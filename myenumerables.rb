9c9c57382b1bc4e7cb1cc7d9cc62a3fa5fed913e
chirchir <chirchir7370@gmail.com>
2020-05-27 19:09:13 +0300

    added my_each_with_index

--- myenumerables.rb
+++ myenumerables.rb
@@ -1,13 +1,20 @@
 module Enumerable
-   def my_each
+  def my_each
     if block_given?
-        counter = 0
-        while counter<self.length
-            yield self[counter]
-            counter+=1
-        end
-    else
-        self
+      counter = 0
+      while counter < length
+        yield self[counter]
+        counter += 1
+      end
     end
-  end
+    self
+ end
+
+  def my_each_with_index
+    index = -1
+    my_each do |value|
+      yield value, index += 1
+    end
+    self
+    end
 end
--- test.rb
+++ test.rb
@@ -1,4 +1,4 @@
 require_relative 'myenumerables.rb'
 include Enumerable
 
-[1,2,3,4].my_each { |g| puts g}
+[1, 2, 3, 4].my_each_with_index { |v, k| puts "#{v}- #{k}" }
